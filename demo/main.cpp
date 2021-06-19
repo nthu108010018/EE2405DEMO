0e6c3ca6850614dd0f62582ee6794ae35c9580c6
nthu108010018 <henry.hung.taiwan@gapp.nthu.edu.tw>
2021-06-19 00:45:10 -0700

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -17,71 +17,43 @@ BufferedSerial uart(D1,D0); //tx,rx
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread pingthread(osPriorityNormal);
 void angle_cal(float angle);
+void car_circle(void);
 void pingf(void);
-char recv[1];
 char angbuf[32];
 float val = 0;
 float angle = 0;
+void RPC_linedetect (Arguments *in, Reply *out);
+RPCFunction RPC_line(&RPC_linedetect, "line");
+
 
 int main(){
    uart.set_baud(9600);
    int i = 0;
    char buf[256], outbuf[256];
-   FILE *devin = fdopen(&pc, "r");
-   FILE *devout = fdopen(&pc, "w");
-   //float angle = 0;
+   FILE *devin = fdopen(&uart, "r"); // uart
+   FILE *devout = fdopen(&uart, "w"); //uart
    queue.call(pingf);
    pingthread.start(callback(&queue, &EventQueue::dispatch_forever));
+   printf("demo start\r\n");
    while(1){
-      
-      if(uart.readable()){
-            uart.read(recv, sizeof(recv));
-            if(recv[0] == '\n'){
-                angle = 0;
-                angle = atof(angbuf);
-                //xbee.write(angbuf, sizeof(angbuf));
-                angle_cal(angle);
-                for(int j = 0; j++; j<32){
-                    angbuf[j] = '0';
-                }
-                i = 0;
-            }
-            else{
-                angbuf[i] = recv[0];
-                i++;
-            }
-            
-            
-      }
-      else{
+    if(val*17700.4f<10){
           car.stop();
-      }
-   }
-}
-
-void angle_cal(float angle){
-   
-
-
-    if(angle>10){
-        car.turn(100, -0.3);
-        ThisThread::sleep_for(250ms);
-        car.stop();
-        //printf("turn right\r\n");
+          car_circle();
     }
-    else if(angle<-10){
-        car.turn(100, 0.3);
-        ThisThread::sleep_for(250ms);
-        car.stop();
-        //printf("turn left\r\n");
+    else if(uart.readable()){
+            memset(buf, 0, 256);      // clear buffer
+            for(int i=0; i<255; i++){
+                char recv = fgetc(devin);
+                if (recv == '\n') {
+                    buf[i] = fputc(recv, devout);
+                    printf("\r\n");
+                    break;
+                }
+                buf[i] = fputc(recv, devout);
+            }
+            RPC::call(buf, outbuf);
+        }
     }
-    else{
-        car.goStraight(100);
-        ThisThread::sleep_for(1000ms);
-        car.stop();
-        //printf("go straight\r\n");
-    }
-   
 }
 
 void pingf(void){
@@ -98,14 +70,56 @@ void pingf(void){
       t.start();
       while(ping.read() == 1);
       val = t.read();
-      //printf("Ping = %lf\r\n", val*17700.4f);
-      char val_buf[40];
-      sprintf(val_buf,"r :%f  theta: %f\r\n",val*17700.4f , angle);
-      
-      xbee.write(val_buf, 40);
       t.stop();
       t.reset();
+      //printf("ping %f\r\n", val*17700.4f);
 
-      ThisThread::sleep_for(1s);
+      ThisThread::sleep_for(100ms);
    }
+}
+
+
+void angle_cal(float angle){
+        printf("%f\r\n", angle);
+
+        if(angle>10){
+            car.turn(100, -0.3);
+            ThisThread::sleep_for(250ms);
+            car.stop();
+            //printf("turn right\r\n");
+        }
+        else if(angle<-10){
+            car.turn(100, 0.3);
+            ThisThread::sleep_for(250ms);
+            car.stop();
+            //printf("turn left\r\n");
+        }
+        else{
+            car.goStraight(100);
+            ThisThread::sleep_for(1000ms);
+            car.stop();
+            //printf("go straight\r\n");
+        }
+}
+
+void car_circle(void){
+    printf("turn\r\n");
+    car.turn(200, -0.3);
+    ThisThread::sleep_for(1000ms);
+    car.stop();
+
+    car.goStraight(100);
+    ThisThread::sleep_for(500ms);
+    car.stop();
+
+    car.turn(200, 0.3);
+    ThisThread::sleep_for(6000ms);
+    car.stop();
+    return;
+}
+
+void RPC_linedetect (Arguments *in, Reply *out)   {
+    angle = in->getArg<float>();
+    angle_cal(angle);
+    return;
 }
