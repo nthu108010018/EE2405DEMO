25b92692400340182d964a5ebfe8300308a7c4cb
nthu108010018 <henry.hung.taiwan@gapp.nthu.edu.tw>
2021-06-19 09:37:52 -0700

    update

--- demo/main.cpp
+++ demo/main.cpp
@@ -20,11 +20,12 @@ void angle_cal(float angle);
 void car_circle(void);
 void pingf(void);
 char angbuf[32];
-float val = 0;
+float val = 1000;
 float angle = 0;
 void RPC_linedetect (Arguments *in, Reply *out);
 RPCFunction RPC_line(&RPC_linedetect, "line");
-
+void RPC_apriltag (Arguments *in, Reply *out);
+RPCFunction RPC_april(&RPC_apriltag, "apriltag");
 
 int main(){
    uart.set_baud(9600);
@@ -35,10 +36,11 @@ int main(){
    queue.call(pingf);
    pingthread.start(callback(&queue, &EventQueue::dispatch_forever));
    while(1){
-    if(val*17700.4f< 15){
+    if(val*17700.4f< 20){
           xbee.write("object detected\r\n", 18);
           car.stop();
           car_circle();
+          ThisThread::sleep_for(1s);
     }
     else if(uart.readable()){
             memset(buf, 0, 256);      // clear buffer
@@ -74,7 +76,7 @@ void pingf(void){
       t.reset();
       //printf("ping %f\r\n", val*17700.4f);
 
-      ThisThread::sleep_for(100ms);
+      ThisThread::sleep_for(10ms);
    }
 }
 
@@ -82,32 +84,62 @@ void pingf(void){
 void angle_cal(float angle){
         xbee.write("line detected\r\n", 16);
 
-        if(angle>20){
-            car.turn(100, -0.3);
+        if(angle>30){
+            car.turn(100, 0.3);
             ThisThread::sleep_for(250ms);
             car.stop();
             //printf("turn right\r\n");
         }
-        else if(angle<-20){
-            car.turn(100, 0.3);
+        else if(angle<-30){
+            car.turn(100, -0.3);
             ThisThread::sleep_for(250ms);
             car.stop();
             //printf("turn left\r\n");
         }
         else{
             car.goStraight(100);
-            ThisThread::sleep_for(1000ms);
+            ThisThread::sleep_for(250ms);
             car.stop();
             //printf("go straight\r\n");
         }
+        ThisThread::sleep_for(500ms);
 }
 
 void car_circle(void){
-    printf("turn\r\n");
     car.turn(200, 0.1);
     ThisThread::sleep_for(1800ms);
     car.stop();
     ThisThread::sleep_for(500ms);
+    /*car.turn(200, 0.1);
+    ThisThread::sleep_for(800ms);
+    car.stop();
+    ThisThread::sleep_for(500ms);
+    car.turn(200, -0.1);
+    ThisThread::sleep_for(1000ms);
+    car.stop();
+    ThisThread::sleep_for(500ms);
+    
+    car.goStraight(200);
+    ThisThread::sleep_for(1000ms);
+
+
+    car.turn(200, -0.1);
+    ThisThread::sleep_for(1000ms);
+    car.stop();
+    ThisThread::sleep_for(500ms);
+    
+    car.goStraight(200);
+    ThisThread::sleep_for(1000ms);
+
+
+    car.stop();
+    ThisThread::sleep_for(500ms);
+
+    car.turn(200, -0.1);
+    ThisThread::sleep_for(1000ms);
+    car.stop();
+    ThisThread::sleep_for(500ms);*/
+
 
     return;
 }
@@ -115,5 +147,19 @@ void car_circle(void){
 void RPC_linedetect (Arguments *in, Reply *out)   {
     angle = in->getArg<float>();
     angle_cal(angle);
+    return;
+}
+
+void RPC_apriltag (Arguments *in, Reply *out)   {
+    float x = in->getArg<float>();
+    float z = in->getArg<float>();
+    
+    char temp[100];
+    xbee.write("Apriltag detected\r\n", 20);
+    sprintf(temp, "x:%f z:%f\r\n", x*6.2, z*6.2);
+    xbee.write(temp, 80);
+    car.stop();
+    ThisThread::sleep_for(100s);
+
     return;
 }
